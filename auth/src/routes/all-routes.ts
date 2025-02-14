5f5da312172a6888d7ff85a365870da202a11532
vinayak <vinayak.kurugovina@gmail.com>
2025-02-15 01:17:39 +0530

    signin completed

--- auth/src/routes/all-routes.ts
+++ auth/src/routes/all-routes.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import { body } from 'express-validator'
 
 import { currentUserHandler, signinHandler, signoutHandler, signupHandler } from '../routesHandlers/authHandler'
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router()
 
-const emailValidator = [
+const emailValidatorSignup = [
   body('email')
     .isEmail()
     .withMessage('Email must be valid'),
@@ -15,12 +16,17 @@ const emailValidator = [
     .withMessage('Password must be between 4 and 20 characters')
 ]
 
+const emailValidatorSignin = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Please enter a valid password')
+]
+
 router.get('/api/v1/users/currentuser', currentUserHandler)
-router.post('/api/v1/users/signin', signinHandler)
+router.post('/api/v1/users/signin', emailValidatorSignin, validateRequest, signinHandler)
 router.post('/api/v1/users/signout', signoutHandler)
 // added email validation as an Arguments
-router.post('/api/v1/users/signup', emailValidator, signupHandler) 
+router.post('/api/v1/users/signup', emailValidatorSignup, validateRequest, signupHandler)
 
-export { 
+export {
   router as authRouter
 };
--- auth/src/routesHandlers/authHandler.ts
+++ auth/src/routesHandlers/authHandler.ts
@@ -1,17 +1,43 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken'
-import { validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+// import { validationResult } from 'express-validator';
+// import { RequestValidationError } from '../errors/request-validation-error';
 // import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const currentUserHandler = (req: Request, res: Response) => {
   res.send('current user');
 };
 
-const signinHandler = (req: Request, res: Response) => {
-  res.send('signin....');
+const signinHandler = async (req: Request, res: Response) => {
+  /*
+    Snippet not required as validateRequest middleware is added in routes
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) throw new RequestValidationError(errors.array())
+  */
+
+  const { email, password } = req.body
+
+  const existingUser = await User.findOne({ email: email })
+  if (!existingUser) throw new BadRequestError('Invalid credentials')
+
+  const passwordMatch = await Password.compare(existingUser.password, password)
+  if (!passwordMatch) throw new BadRequestError('Invalid credentials')
+
+  // Generate JWT
+  const userJWT = jwt.sign(
+    { id: existingUser.id, email: existingUser.email },
+    process.env.JWT_KEY! // Readme: Saving secrets in kubernates pods
+  )
+
+  // Store JWT on session object
+  req.session = {
+    jwt: userJWT
+  }
+
+  res.status(200).send(existingUser);
 };
 
 const signoutHandler = (req: Request, res: Response) => {
@@ -19,13 +45,16 @@ const signoutHandler = (req: Request, res: Response) => {
 };
 
 const signupHandler = async (req: Request, res: Response) => {
-  const errors = validationResult(req);
+  /*
+    Snippet not required as validateRequest middleware is added in routes
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-    // throw new Error('Invalid email or password')
-    // return res.status(400).send(errors.array())
-  }
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+      // throw new Error('Invalid email or password')
+      // return res.status(400).send(errors.array())
+    }
+  */
 
   const { email, password } = req.body;
 
