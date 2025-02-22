db0b5457ad51a47a0a3b0e6368e3650e6e5d65ff
vinayak <vinayak.kurugovina@gmail.com>
2025-02-22 12:14:51 +0530

    Listener -> listening for data, client id generation, queue groups, manual ack mode, client shutdown, durable subscriptions

--- nats-test/ReadMe.md
+++ nats-test/ReadMe.md
@@ -11,3 +11,7 @@ kubectl get pods
 kubectl port-forward POD_NAME PORT_LOCAL_MACHINE : PORT_ON_POD
 
 kubectl port-forward nats-depl-6d9bdd744f-5x9v8 4222:4222
+kubectl port-forward nats-depl-6d9bdd744f-5x9v8 8222:8222
+
+http://localhost:8222/streaming
+http://localhost:8222/streaming/channelsz?subs=1
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,18 +1,40 @@
-import nats from 'node-nats-streaming'
+import { SubscribeOptions } from './../node_modules/nats/index.d';
+import { randomUUID } from 'crypto'
+import nats, { Message, Stan } from 'node-nats-streaming'
 
 console.clear()
 
-const client = nats.connect('ticketing', '123', {
-  url: 'http://localhost:4222'
-})
+// const clientId = randomBytes(4).toString().replace(/[^a-zA-Z0-9]/g, "") + 'id'
+const clientId = randomUUID()
+const client = nats.connect('ticketing', clientId, { url: 'http://localhost:4222' })
 
 client.on('connect', () => {
   console.log('Listener connected to NATS...')
 
+  client.on('close', () => {
+    console.log('NATS connection closed...!')
+    process.exit()
+  })
 
-  const subscription = client.subscribe('ticket:creted')
 
-  subscription.on('message', (msg) => {
-    console.log("Message received")
+  const options = client
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('order-service')
+
+  // topic/channel, queue group, options
+  const subscription = client.subscribe('ticket:creted', 'order-service-queue-group', options)
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData()
+    if (typeof data === 'string') {
+      console.log(`Received event # ${msg.getSequence()}, with data: ${data}`)
+    }
+
+    msg.ack()
   })
 })
+
+process.on('SIGINT', () => client.close()) // interrupt
+process.on('SIGTERM', () => client.close()) // terminate
