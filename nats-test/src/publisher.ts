f794851172bcfe0e32f8ebbeb33c418a25e33317
vinayak <vinayak.kurugovina@gmail.com>
2025-02-22 14:53:41 +0530

    abstract Publisher class created and refactored code

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming'
+import { TicketCreatedPublisher } from './events/ticket-created-publisher'
 
 console.clear()
 
@@ -6,14 +7,19 @@ const client = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222'
 })
 
-client.on('connect', () => {
-  console.log('Publisher connected to NATS...')
+client.on('connect', async () => {
+  console.log('Publisher connected to NATS...!')
+  const data = { id: '123', title: 'connect', price: 20 }
 
-
-  const data = JSON.stringify({ id: '123', title: 'connect', price: 20 })
-
-  client.publish('ticket:created', data, () => {
+  const publisher = new TicketCreatedPublisher(client)
+  try {
+    await publisher.publish(data)
+  } catch (error) {
+    console.error('Error ', error)
+  }
+  /*
+  client.publish('ticket:created', JSON.stringfy(data), () => {
     console.log('Event published')
   })
-
+  */
 })
