a36d5e99893ce47d4d9253ce774a064fa40abfcd
vinayak <vinayak.kurugovina@gmail.com>
2025-02-26 00:42:04 +0530

    version update for order model

--- orders/src/models/orders.ts
+++ orders/src/models/orders.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { OrderStatus } from "@vktickets/shared";
 import { TicketDoc } from "./ticket";
 
@@ -31,6 +32,9 @@ const orderSchema = new mongoose.Schema({
     }
   }
 });
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin)
 
 orderSchema.statics.build = (attrs: OrderAttrs) => new Order(attrs)
 
